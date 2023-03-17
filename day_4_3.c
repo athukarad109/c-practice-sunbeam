0842001622d28be036ae3aa690bd22c3a6decf2b
Atharv Kulkarni <68152380+athukarad109@users.noreply.github.com>
2023-03-17 13:12:21 +0530

    adding scanf

---
+++ day_4_3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    char ch;
+    scanf("%c%d", &ch, &num);
+    printf("Value is :%c %d", ch, num);
+    return 0;
+}
+
