998a648ec0cd0e50d1130c3cb92bb3f2fa3cbe7b
Atharv Kulkarni <68152380+athukarad109@users.noreply.github.com>
2023-03-27 15:06:36 +0530

    Pointers and rec

---
+++ pointer.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int n = 10;
+    int *ptr = &n; //referencing
+    int **addptr = &ptr; //referencing pointer
+    printf("%d\n", ptr);
+    printf("%d\n", *ptr); //*ptr -> value at ptr
+    printf("%d\n", **addptr);
+
+    *ptr = 20; //updating value by pointer
+    printf("%d\n", n);
+
+    n = 15;
+    printf("%d\n", *ptr); //*ptr -> value at ptr
+    printf("%d\n", ptr);
+
+
+
+    return 0;
+}
